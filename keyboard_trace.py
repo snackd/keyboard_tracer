4a729fc136882e54fad955feeccdf5434238b042
snackd <snackd@gmail.com>
2023-01-19 16:43:02 +0800

    feat:print color & OOP

--- keyboard_trace.py
+++ keyboard_trace.py
@@ -1,37 +1,61 @@
-# https://pynput.readthedocs.io/en/latest/keyboard.html
 from pynput import keyboard
+from termcolor import cprint
 
-def on_press(key):
-    try:
-        print('Alphanumeric key pressed: {0} '.format(
-            key.char))
-    except AttributeError:
-        print('special key pressed: {0}'.format(
+
+class KeyboardTracer:
+    _instance = None
+
+    def __new__(cls, *args, **kwargs):
+        if cls._instance is None:
+            cls._instance = super().__new__(cls)
+        return cls._instance
+
+    def __init__(self):
+        self.flag = 1
+
+    def on_press(self, key):
+        try:
+            print('Alphanumeric key pressed: {0} '.format(
+                key.char))
+        except AttributeError:
+            print('special key pressed: {0}'.format(
+                key))
+
+    def on_release(self, key):
+        print('Key released: {0}'.format(
             key))
+        if key == keyboard.Key.esc:
+            self.flag = 0
+            cprint("Release ESC, Stop Listener", "red")
+            # Stop listener
+            return False
 
-def on_release(key):
-    print('Key released: {0}'.format(
-        key))
-    if key == keyboard.Key.esc:
-        # Stop listener
-        return False
+    def collector(self):
+        # Collect events until released
+        with keyboard.Listener(
+                on_press=on_press,
+                on_release=on_release) as listener:
+            listener.join()
 
-def collector():
-    # Collect events until released
-    with keyboard.Listener(
-            on_press=on_press,
-            on_release=on_release) as listener:
-        listener.join()
+    def main(self):
+        print("-"*10)
+        cprint("Program Start", "green")
+        print("-"*10)
 
-def main():
-    print("---Program Start---")
+        # ...or, in a non-blocking fashion:
+        listener = keyboard.Listener(
+            on_press=self.on_press,
+            on_release=self.on_release)
+        listener.start()
 
-    # ...or, in a non-blocking fashion:
-    listener = keyboard.Listener(
-        on_press=on_press,
-        on_release=on_release)
-    listener.start()
 
 if __name__ == "__main__":
-    main()
+    k = KeyboardTracer()
+    k.main()
 
+    while k.flag:
+        pass
+
+    print("-"*10)
+    cprint("End Program", "green")
+    print("-"*10)
